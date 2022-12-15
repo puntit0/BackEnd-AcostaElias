806f08e85a043970e19183d0b76c0f5a19bf0a00
puntit0 <elias2016acosta@gmail.com>
2022-12-14 22:16:25 -0300

    corrigiendo CORS en el back

--- src/main/java/com/Elias/Acosta/arg/Controller/CExperiencia.java
+++ src/main/java/com/Elias/Acosta/arg/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://hosting-angular-argprog-bee03.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
