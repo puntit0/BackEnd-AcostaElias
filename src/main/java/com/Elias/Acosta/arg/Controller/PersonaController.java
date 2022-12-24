51eb19ddc0ac659dcf8750dc2457143d6e76bdbe
puntit0 <elias2016acosta@gmail.com>
2022-12-24 14:04:07 -0300

    deploy con hys

--- src/main/java/com/Elias/Acosta/arg/Controller/PersonaController.java
+++ src/main/java/com/Elias/Acosta/arg/Controller/PersonaController.java
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://proyecfrontend.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://proyecfrontend.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
--- src/main/java/com/Elias/Acosta/arg/Security/Controller/AuthController.java
+++ src/main/java/com/Elias/Acosta/arg/Security/Controller/AuthController.java
@@ -34,8 +34,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://proyecfrontend.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://proyecfrontend.web.app")
 public class AuthController {
 
     @Autowired
